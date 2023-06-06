1ce9d8168ffb24cb1efdda95fe6d2f637a35f7c3
Michael Adams <kirosaima@protonmail.com>
2023-06-06 22:05:23 +0100

    change category page title to include category

--- app/(site)/category/[category]/page.tsx
+++ app/(site)/category/[category]/page.tsx
@@ -11,7 +11,7 @@ export default async function Articles({ params }: Props) {
 
   return (
     <div className="max-w-5xl mx-5 md:mx-auto py-10">
-      <h1 className="text-white text-5xl font-extrabold">Articles</h1>
+      <h1 className="text-white text-5xl font-extrabold">{category}</h1>
       <div className="mt-5 grid md:grid-cols-2 lg:grid-cols-3 gap-8">
         {posts.map((post) => (
           <Link
